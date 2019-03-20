e41fbb7f765cf5e81a1914e56b250d4c47ef83d8
Arman <poghosyanarman174@mail.ru>
2019-03-20 21:25:52 +0400

    Collections

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,41 @@ namespace Collections
     {
         static void Main(string[] args)
         {
+            UserCollection myCollection = new UserCollection();
+
+            foreach (Element element  in myCollection)
+            {
+                Console.WriteLine($"Name: {element.Name}  Field1: {element.Field1}  Field2: {element.Field2}");
+            }
+
+            //myCollection.Reset();
+
+            Console.WriteLine(new string('-', 29) + "\n");
+
+            foreach (Element element in myCollection)
+            {
+                Console.WriteLine($"Name: {element.Name}  Field1: {element.Field1}  Field2: {element.Field2}");
+            }
+
+            Console.WriteLine(new string('-', 29) + "\n");
+
+
+            UserCollection myElementsCollection = new UserCollection();
+
+            IEnumerable enumerable = myElementsCollection as IEnumerable;
+
+            IEnumerator enumerator = enumerable.GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                Element element = enumerator.Current as Element;
+
+                Console.WriteLine($"Name: {element.Name}  Field1: {element.Field1}  Field2: {element.Field2}");
+            }
+
+            enumerator.Reset();
+
+            Console.ReadKey();
         }
     }
 }
